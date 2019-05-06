85b01877081ba4a0e1ec914f252f61c1188c0a4d
Njuguna84 <dennix.njuguna85@gmail.com>
2019-05-06 17:50:05 +0300

    Mappinng with folium

--- app2/mapping.py
+++ app2/mapping.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import json
 # mapping library
 import folium
 
@@ -11,11 +11,20 @@ long = list(data["LON"])
 elevation = list(data["ELEV"])
 name = list(data["NAME"])
 
+"""
+    Creating a new map with its centre at the location list
+    its tile layers as Stamen and its zoom control
+"""
 map = folium.Map(
     location=[35.0902, -95.7129],
     tiles='Stamen Toner',
-    zoom_control=3,
+    zoom_start=3,
 )
+
+"""
+        the function accepts elevation parameter
+        and will categorized it and return a color
+"""
 
 
 def categorized_color(elevation):
@@ -29,18 +38,59 @@ def categorized_color(elevation):
         return 'red'
 
 
-feature_Group = folium.FeatureGroup(name="My Map")
+# Creating a new feature group
+fg_Markers = folium.FeatureGroup(name="Volcanoes")
+
+"""
+        Creating new markers
+        We use zip function to match the objects together to a list
+    """
+# for lt, ln, nm, el, in zip(lat, long, name, elevation):
+#     fg_Markers.add_child(
+#         folium.Marker(
+#             location=[lt, ln],
+#             popup="<b>Name: </b>" + nm + '\n' +
+#             "<b>Height: </b>" + str(el) + 'm',
+#             icon=folium.Icon(color=categorized_color(el))
+#         )
+#     )
+
+# create new circle Markers
+fg_CircleMarkers = folium.FeatureGroup(name="Volcanoes")
 
 for lt, ln, nm, el, in zip(lat, long, name, elevation):
-    feature_Group.add_child(
-        folium.Marker(
+    fg_CircleMarkers.add_child(
+        folium.CircleMarker(
             location=[lt, ln],
+            radius=5,
+            fill=True,
+            fill_color=categorized_color(el),
             popup="<b>Name: </b>" + nm + '\n' +
-            "<b>Height: </b>" + str(el) + 'm',
-            icon=folium.Icon(color=categorized_color(el))
-        )
+            "<b>Height: </b>" + str(el) + 'm', color=categorized_color(el))
     )
 
-map.add_child(feature_Group)
+"""
+    Categorized country layer color depending on the population
+    Using lambda function and if else statement
+    """
+fg_Poly = folium.FeatureGroup(name = "World Population")
+fg_Poly.add_child(
+    folium.GeoJson(
+        data=open("world.json", 'r',
+                  encoding='utf-8-sig').read(),
+        style_function=lambda z:
+        {'fillColor': 'yellow'
+         if z['properties']['POP2005'] < 10000000
+         else 'orange' if 10000000 <= z['properties']['POP2005'] < 20000000
+         else 'red'}))
+
+
+map.add_child(fg_Markers)
+map.add_child(fg_CircleMarkers)
+map.add_child(fg_Poly)
+
+# layer control
+# this is added after adding feature groups to the map
+map.add_child(folium.LayerControl())
 
 map.save('map.html')
